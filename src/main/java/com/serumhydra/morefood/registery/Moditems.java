711fb8232e066f39d13dd8203a6bfc41917de975
SerumHydra <81027341+SerumHydra@users.noreply.github.com>
2021-08-26 00:17:42 +0100

    update: mincer added to creative tab, and also name sorted, just need to figure out functionality

--- src/main/java/com/serumhydra/morefood/registery/Moditems.java
+++ src/main/java/com/serumhydra/morefood/registery/Moditems.java
@@ -3,10 +3,12 @@ package com.serumhydra.morefood.registery;
 import com.serumhydra.morefood.MoreFood;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.lwjgl.system.CallbackI;
 
 public class Moditems {
     //items
@@ -20,9 +22,10 @@ public class Moditems {
     public static final Item MONSTER_ORIGINAL = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED,20*10),1f).statusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS,20*12),1f).build()));
     public static final Item DINO_NUGGIES = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(6).saturationModifier(6f).build()));
     //Block Items
-
+    public static final BlockItem MINCER = new BlockItem(ModBlocks.MINCER, new Item.Settings().group(MoreFood.FOOD_GROUP));
     //registry
     public static void registerItems() {
+        //register items
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "cooked_bacon"), COOKED_BACON);
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "ice_cream"), ICE_CREAM);
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "noodles"), NOODLES);
@@ -32,5 +35,7 @@ public class Moditems {
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "coffee"), COFFEE);
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "monster_energy_original"), MONSTER_ORIGINAL);
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "dino_nuggies"), DINO_NUGGIES);
+        //register Block Items
+        Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "mincer"), MINCER);
     }
 }
