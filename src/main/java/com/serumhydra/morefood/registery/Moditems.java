5b22e767dc4f4e5a963813a161da145f0e0a722f
SerumHydra <81027341+SerumHydra@users.noreply.github.com>
2021-08-26 00:17:42 +0100

    update: mincer added to creative tab, and also name sorted, just need to figure out functionality

--- src/main/java/com/serumhydra/morefood/registery/Moditems.java
+++ src/main/java/com/serumhydra/morefood/registery/Moditems.java
@@ -8,6 +8,7 @@ import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.lwjgl.system.CallbackI;
 
 public class Moditems {
     //items
@@ -16,22 +17,25 @@ public class Moditems {
     public static final Item NOODLES = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(4).saturationModifier(6f).build()));
     public static final Item RAW_BACON = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(2).saturationModifier(2f).build()));
     public static final Item MEATBALLS = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(2f).build()));
-    public static final Item ITALIAN_BREAD = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(3).saturationModifier(4f).build()));
     public static final Item MEATBALL_SANDWHICH = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(6).saturationModifier(8f).build()));
     public static final Item COFFEE = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED,20*5),1f).build()));
-    public static final Item MONSTER = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED,20*10),1f).build()));
+    public static final Item MONSTER_ORIGINAL = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(1).saturationModifier(0f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED,20*10),1f).statusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS,20*12),1f).build()));
+    public static final Item DINO_NUGGIES = new Item(new Item.Settings().group(MoreFood.FOOD_GROUP).food(new FoodComponent.Builder().hunger(6).saturationModifier(6f).build()));
     //Block Items
-
+    public static final BlockItem MINCER = new BlockItem(ModBlocks.MINCER, new Item.Settings().group(MoreFood.FOOD_GROUP));
     //registry
     public static void registerItems() {
+        //register items
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "cooked_bacon"), COOKED_BACON);
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "ice_cream"), ICE_CREAM);
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "noodles"), NOODLES);
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "raw_bacon"), RAW_BACON);
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "meatballs"), MEATBALLS);
-        Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "italian_bread"), ITALIAN_BREAD);
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "meatball_sandwhich"), MEATBALL_SANDWHICH);
         Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "coffee"), COFFEE);
-        Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "monster_energy"), MONSTER);
+        Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "monster_energy_original"), MONSTER_ORIGINAL);
+        Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "dino_nuggies"), DINO_NUGGIES);
+        //register Block Items
+        Registry.register(Registry.ITEM, new Identifier(MoreFood.MOD_ID, "mincer"), MINCER);
     }
 }
